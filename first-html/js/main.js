160d72d9eb27cb5a0352aa44ee9b82edd42e96dd
FaiqTech <hesenovfaiq@icloud.com>
2023-12-09 12:38:10 +0400

    first commit

---
+++ first-html/js/main.js
@@ -0,0 +1,26 @@
+// vribale with welector
+const list = document.querySelector("#infinite-list");
+var itemNumber = 0
+// Add 20 items.
+function loadItems() {
+    for (let i = 1; i <= 10; i++) {
+        itemNumber ++
+        const element = document.createElement('li')
+        element.innerText = `${itemNumber} - indexli li elementi`;
+        list.appendChild(element)
+    }
+}
+
+// Detect when scrolled to bottom.
+// scrollTop; // scrollHeight; // clientHeight
+list.addEventListener("scroll", () => {
+    console.log(list.scrollTop, "scrollTop");
+    console.log(list.scrollHeight, "scrollHeight");
+    console.log(list.clientHeight, "clientHeight");
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight && itemNumber<100) {
+        loadItems();
+    }
+});
+
+// Initially load some items.
+loadItems()
